068379cab37b6f3183668569f83add7486e5c437
Maxim Izosimov <maxizosimov@gmail.com>
2025-05-20 21:07:30 -0400

    deploying changs

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/maxizosimov.github.io/',
+  base: '/', // Change this since it's the root domain
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
